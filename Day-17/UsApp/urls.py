30f467d8f0960330aa9e91617cd862dfdea3d235
NRK <rravikumar34@gmail.com>
2020-12-08 12:31:02 +0530

    Django Day-17 Updated

---
+++ Day-17/UsApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from UsApp import views
+from django.contrib.auth import views as v
+
+urlpatterns = [
+    path('',views.home,name="hm"),
+    path('abt/',views.abts,name="ab"),
+    path('cnt/',views.cntc,name="cn"),
+    path('reg/',views.regis,name="rg"),
+    path('lg/',v.LoginView.as_view(template_name="sa/login.html"),name="log"),
+    path('ds/',views.dashboard,name="dsh"),
+    path('lgo/',v.LogoutView.as_view(template_name="sa/logout.html"),name="lgg"),
+]
